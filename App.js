ffa6c5d04ae4b9b93390b70fb082f5586e57438e
annagao <ggaoyanyan@gmail.com>
2021-12-20 22:02:40 -0800

    Icons, Favorites, and Comments

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
 import Main from './components/MainComponent';
 
 export default function App() {
     return (
+        <SafeAreaView style={{flex:1}}>
         <Main />
+        </SafeAreaView>
+        
     );
 }
